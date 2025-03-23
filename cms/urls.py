465176d8d9ad2ce1fe93de01c8c358af77fa3dcc
ASt4Github <adam.steiner@student.gyarab.cz>
2025-03-23 14:27:26 +0100

    Pokus s obrázky

--- cms/urls.py
+++ cms/urls.py
@@ -19,7 +19,6 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
-
 urlpatterns = [
     path('', include('content.urls')),
     path('admin/', admin.site.urls),
